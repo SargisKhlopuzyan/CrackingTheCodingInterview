7ecb41da45cf6d00275b8fde5f1b79dda8312c9c
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2020-01-27 19:15:28 +0400

    Example_16

---
+++ app/src/main/java/com/example/crackingthecodinginterview/Example_16.kt
@@ -0,0 +1,20 @@
+package com.example.crackingthecodinginterview
+
+
+fun main() {
+    powersOf2_16(127)
+}
+
+fun powersOf2_16(n: Int): Int {
+    if (n < 1) {
+        return 0
+    } else if (n == 1) {
+        println(1)
+        return 1
+    } else {
+        var prev = powersOf2_16(n / 2)
+        var curr = prev * 2
+        println(curr)
+        return curr
+    }
+}
