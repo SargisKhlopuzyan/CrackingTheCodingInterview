bf3b4abf679ee15dedf39d5d717b4d2e63285db7
Sargis Khlopuzyan <sargis.khlopuzyan@org.fcc.am>
2020-01-30 17:57:23 +0400

    Optimize & Solve Technique #2: DIY (Do It Yourself)

---
+++ app/src/main/java/com/example/crackingthecodinginterview/vii_technical_questions/PrintTheLocationOfEachPermutation.kt
@@ -0,0 +1,38 @@
+package com.example.crackingthecodinginterview.vii_technical_questions
+
+/**
+ * Created by Sargis Khlopuzyan, on 1/30/2020.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fcc.am)
+ */
+
+fun main() {
+    val s = "abbc"
+    val b = "cbabadcbbabbcbabaabccbabc"
+
+    printTheLocationOfEachPermutation(s, b)
+}
+
+fun printTheLocationOfEachPermutation(s: String, b: String) {
+
+    for ((index, bb) in b.withIndex()) {
+
+    }
+
+    for ((index, ss) in s.withIndex()) {
+        println("index: $index, ss: $ss")
+
+    }
+}
+
+fun checIsArraysEqual(array1: Array<String>, array2: Array<String>) {
+
+    var hashMap = HashMap<String, Int>()
+
+    for (element in array1) {
+
+        val value = element[element]
+        if (has)
+    }
+
+}
