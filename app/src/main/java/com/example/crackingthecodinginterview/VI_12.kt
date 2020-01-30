0a83a9480ac1e03dfa5f44c30243530778395677
Sargis Khlopuzyan <sargis.khlopuzyan@org.fcc.am>
2020-01-30 09:11:21 +0400

    VI_12
    Quick Sort

---
+++ app/src/main/java/com/example/crackingthecodinginterview/VI_12.kt
@@ -0,0 +1,119 @@
+package com.example.crackingthecodinginterview
+
+/**
+ * Created by Sargis Khlopuzyan, on 1/29/2020.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fcc.am)
+ */
+
+fun main() {
+    var a = arrayOf(6, 9, 2, 5, 4, 2)
+    var b = arrayOf(1, 2, 5, 6, 3, 2)
+
+    intersection_vi_12(a, b)
+}
+
+fun intersection_vi_12(a: Array<Int>, b: Array<Int>) {
+
+    var insertion = 0
+
+    println("b: ${b.asList()}")
+//    bubbleSort_vi_12(b)
+    quickSort_vi_12(b)
+    println("b: ${b.asList()}")
+
+    for (i in a) {
+        if (binarySearch_vi_12(b, i) >= 0) {
+            insertion++
+        }
+    }
+
+    println("insertion: $insertion")
+}
+
+// Quick Sort
+
+fun quickSort_vi_12(arr: Array<Int>) {
+    quickSort_vi_12(arr, 0, arr.size - 1)
+}
+
+fun quickSort_vi_12(arr: Array<Int>, lowIndex: Int, highIndex: Int) {
+
+    if (lowIndex < highIndex) {
+        var partitionIndex = partition_vi_12(arr, lowIndex, highIndex)
+        quickSort_vi_12(arr, lowIndex, partitionIndex - 1)
+        quickSort_vi_12(arr, partitionIndex + 1, highIndex)
+    }
+
+}
+
+fun partition_vi_12(arr: Array<Int>, lowIndex: Int, highIndex: Int): Int {
+
+    var pivot = arr[highIndex]
+
+    var i = lowIndex - 1
+
+    for (j in lowIndex until highIndex) {
+
+        if (arr[j] < pivot) {
+            i++
+            swap_vi_12(arr, i, j)
+        }
+    }
+
+    swap_vi_12(arr, i + 1, highIndex)
+
+    return i + 1
+}
+
+// Bubble Sort
+
+fun bubbleSort_vi_12(b: Array<Int>) {
+    for (i in 0 until b.size - 1) {
+        for (j in i + 1 until b.size) {
+            if (b[i] > b[j]) {
+                swap_vi_12(b, i, j)
+            }
+        }
+    }
+}
+
+// Swap
+
+fun swap_vi_12(arr: Array<Int>, pos1: Int, pos2: Int) {
+
+    if (pos1 == pos2) {
+        return
+    }
+
+    var t = arr[pos1]
+    arr[pos1] = arr[pos2]
+    arr[pos2] = t
+}
+
+// Binary Search
+
+fun binarySearch_vi_12(b: Array<Int>, value: Int): Int {
+    return binarySearch_vi_12(b, value, 0, b.size - 1)
+}
+
+fun binarySearch_vi_12(array: Array<Int>, value: Int, startIndex: Int, endIndex: Int): Int {
+
+    var midIndex = (startIndex + endIndex) / 2
+
+    if (startIndex > endIndex) {
+        return -1
+    }
+
+    return when {
+        array[midIndex] == value -> {
+            midIndex
+        }
+        array[midIndex] < value -> {
+            binarySearch_vi_12(array, value, midIndex + 1, endIndex)
+        }
+        else -> {
+            binarySearch_vi_12(array, value, startIndex, midIndex - 1)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/crackingthecodinginterview/algorithms/sorting/QuickSort.kt
@@ -0,0 +1,80 @@
+package com.example.crackingthecodinginterview.algorithms.sorting
+
+/**
+ * Created by Sargis Khlopuzyan, on 1/30/2020.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fcc.am)
+ */
+
+fun main() {
+
+    var array: Array<Int> = arrayOf(10, 80, 30, 90, 40, 50, 70)
+    quickSort(array)
+    println("array: ${array.toList()}")
+}
+
+fun quickSort(array: Array<Int>) {
+    quickSort(
+        array,
+        0,
+        array.size - 1
+    )
+}
+
+fun quickSort(array: Array<Int>, lowIndex: Int, highIndex: Int) {
+
+    if (lowIndex < highIndex) {
+
+        var pivot =
+            partition(
+                array,
+                lowIndex,
+                highIndex
+            )
+
+        quickSort(
+            array = array,
+            lowIndex = lowIndex,
+            highIndex = pivot - 1
+        )
+        quickSort(
+            array = array,
+            lowIndex = pivot + 1,
+            highIndex = highIndex
+        )
+    }
+}
+
+
+fun partition(array: Array<Int>, lowIndex: Int, highIndex: Int): Int {
+
+    var pivot = array[highIndex]
+    var i = lowIndex - 1
+
+    for (j in lowIndex until highIndex) {
+
+        if (array[j] < pivot) {
+            i++
+            swap(
+                array,
+                i,
+                j
+            )
+        }
+
+    }
+
+    swap(
+        array,
+        i + 1,
+        highIndex
+    )
+
+    return i + 1
+}
+
+fun swap(array: Array<Int>, position1: Int, position2: Int) {
+    var temp = array[position1]
+    array[position1] = array[position2]
+    array[position2] = temp
+}
