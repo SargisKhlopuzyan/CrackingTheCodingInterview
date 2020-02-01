f44d11af12ad5a3f65e375ed69cbc1bf5d141dbc
Sargis Khlopuzyan <sargis.khlopuzyan@org.fcc.am>
2020-02-01 11:25:31 +0400

    Given two sorted arrays find the number of elements in common
    What's the relationship between the Best Conceivable Runtime and Best Case Runtime? Nothing at all! The Best Conceivable Runtime is for a problem and is largely a function of the inputs and outputs. It has no particular connection to a specific algorithm. In fact, if you compute the Best Conceivable Runtime by thinking about what your algorithm does, you're probably doing something wrong. The Best Case Runtime is for a specific algorithm (and is a mostly useless value).

---
+++ app/src/main/java/com/example/crackingthecodinginterview/vii_technical_questions/Best_Conceivable_Runtime_BCR/GivenTwoSortedArraysFindTheNumberOfElementsInCommon.kt
@@ -0,0 +1,35 @@
+package com.example.crackingthecodinginterview.vii_technical_questions.Best_Conceivable_Runtime_BCR
+
+/**
+ * Created by Sargis Khlopuzyan, on 2/1/2020.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fcc.am)
+ */
+
+fun main() {
+    val array1 = arrayOf(13, 27, 35, 40, 49, 55, 59)
+    val array2 = arrayOf(17, 35, 39, 40, 55, 58, 60)
+
+    findTheNumberOfElementsInCommon(array1, array2)
+}
+
+fun findTheNumberOfElementsInCommon(array1: Array<Int>, array2: Array<Int>) {
+
+    val hashMap1 = HashMap<Int, Int>()
+    val hashMap2 = HashMap<Int, Int>()
+
+    for (i in array1.indices) {
+        hashMap1[array1[i]] = i
+        hashMap2[array2[i]] = i
+    }
+
+    var elementsInCommon = mutableListOf<Int>()
+
+    for (element in array1) {
+        if (hashMap1[element] != null && hashMap2[element] != null) {
+            elementsInCommon.add(element)
+        }
+    }
+
+    println("elementsInCommon: $elementsInCommon")
+}
